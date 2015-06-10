c183f7a46a22884df208d05fa1fc3a3a97c048de
McLeanBH <mcleanbh@gmail.com>
2015-06-10 15:06:46 -0400

    consolidated scss files, added header

--- scripts/main.js
+++ scripts/main.js
@@ -1,3 +1,16 @@
-$(function() {
-  $( "#accordion" ).accordion();
+$(document).ready(function() {
+  var menuToggle = $('#js-mobile-menu').unbind();
+  $('#js-navigation-menu').removeClass("show");
+
+  menuToggle.on('click', function(e) {
+    e.preventDefault();
+    $('#js-navigation-menu').slideToggle(function(){
+      if($('#js-navigation-menu').is(':hidden')) {
+        $('#js-navigation-menu').removeAttr('style');
+      }
+    });
+  });
+  $(function() {
+    $( "#accordion" ).accordion();
+  });
 });
